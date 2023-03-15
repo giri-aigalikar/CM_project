d5c0c6a6e19ab50d64606a7bcf97189de2114d02
Giri Aigalikar <giri.aigalikar@ipg.local>
2023-03-15 14:36:31 -0400

    New Executable

--- JenkinsTest/src/app_tmp.c
+++ JenkinsTest/src/app_tmp.c
@@ -58,10 +58,10 @@ static const char *CompileFlags[] = {
 
 tAppStartInfo   AppStartInfo = {
     "Car_Generic <insert.your.version.no>",          /* App_Version         */
-    "6",          /* App_BuildVersion    */
+    "7",          /* App_BuildVersion    */
     "giri.aigalikar",     /* App_CompileUser     */
     "giri-pc",         /* App_CompileSystem   */
-    "2023-03-15 14:33:32",  /* App_CompileTime */
+    "2023-03-15 14:36:23",  /* App_CompileTime */
 
     CompileFlags,                /* App_CompileFlags  */
     CompileLibs,                 /* App_Libs          */
